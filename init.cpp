c80154a095fd9ad52813477da99cb64a6dc08764
R0bert196 <robertchiriac196@yahoo.com>
2024-11-21 15:39:31 +0200

    add test code in main, initialize autobuz class

--- init.cpp
+++ init.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include "Autobuz.h"
+
 using namespace std;
 
 int main()
 {
-
-    cout << "test robb";
+    try
+    {
+        Autobuz a1(50, 30, "Mercedes");
+        cout << "ASDASD";
+    }
+    catch (const exception &ex)
+    {
+        cerr << "Eroare: " << ex.what() << endl;
+    }
     return 0;
 }
