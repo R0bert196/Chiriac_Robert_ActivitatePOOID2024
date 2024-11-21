e33976c84ec362c7cf0aca88560635475f983453
R0bert196 <robertchiriac196@yahoo.com>
2024-11-21 12:59:35 +0200

    defined overloading operators

--- Autobuz.h
+++ Autobuz.h
@@ -12,8 +12,14 @@ private:
     char *producator;
 
 public:
-    Autobuz();                                                 // Constructor fără parametri
+    Autobuz();                                                 // Constructor fara
     Autobuz(int capacitate, int nrPersoane, const char *prod); // Constructor cu parametri
     Autobuz(const Autobuz &other);                             // Constructor de copiere
     ~Autobuz();                                                // Destructor
+
+    Autobuz &operator=(const Autobuz &other);                                   // Operator= supraincarcat
+    friend std::ostream &operator<<(std::ostream &out, const Autobuz &autobuz); // Operator<<
+
+    operator int() const;                       // Operator cast la int
+    bool operator>(const Autobuz &other) const; // Operator>
 };
